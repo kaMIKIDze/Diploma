292a266b12716c19fbbc2500ac5c03267ec4fdd5
kaMIKIDze <nikitagromo@mail.ru>
2019-04-11 17:21:57 +0700

    Добавили navigation drawer и фрагменты с заглушками

--- app/src/main/java/com/gromov/diploma/NavigationActivity.java
+++ app/src/main/java/com/gromov/diploma/NavigationActivity.java
@@ -1,14 +1,98 @@
 package com.gromov.diploma;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toolbar;
 
-public class NavigationActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class NavigationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private View navHeader;
+
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar((androidx.appcompat.widget.Toolbar) findViewById(R.id.toolbar));
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navHeader = navigationView.getHeaderView(0);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else super.onBackPressed();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer_view, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_purchase) {
+            return true;
+        } else
+            return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_purchase:
+                displaySelectedFragment(new PurchaseFragment());
+                break;
+            case R.id.nav_category:
+                displaySelectedFragment(new CategoryFragment());
+                break;
+            case R.id.nav_analysis:
+                displaySelectedFragment(new AnalysisFragment());
+                break;
+            case R.id.nav_info:
+                displaySelectedFragment(new InfoFragment());
+                break;
+            case R.id.nav_setting:
+                displaySelectedFragment(new SettingFragment());
+                break;
+            default:
+                break;
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+
+    private void displaySelectedFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content_frame_nav, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/gromov/diploma/PurchaseFragment.java
@@ -0,0 +1,17 @@
+package com.gromov.diploma;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class PurchaseFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.purchase_fragment, container, false);
+    }
+}
